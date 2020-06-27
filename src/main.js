c09f643a608b833ecf45fe3a27cb284a5bf25c8b
ft-z <546080688@qq.com>
2020-06-27 23:54:48 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-Vue.config.productionTip = false
+// 根据前端的跨域方式做调整
+axios.defaults.baseURL='/api';
+axios.defaults.timeout=8000;
+// 接口错误拦截
+axios.interceptors.response.use(function (response) {
+  let res=response.data;
+  if(res.status==0){
+    return res.data;
+  }else if(res.status==10){
+    // 井号代表哈希路由
+    window.location.href='/#/login';
+  }else{
+    alert(res.msg);
+  }
+});
+
+Vue.use(VueAxios, aios);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
